c6ae1dbc8a4735dc729c0fd2267e7d8876760073
Halyna Dovhopola <stahova2404@gmail.com>
2023-11-20 14:36:09 +0200

    fix

--- src/scripts/develop/index.js
+++ src/scripts/develop/index.js
@@ -100,58 +100,90 @@ const gallery = new Swiper('.gallery__slider', {
     }
 });
 
-//
-//
-// const validateForm = (form, func) => {
-//     form.on("submit", function (e) {
-//         e.preventDefault();
-//     });
-//
-//     $.validator.addMethod("goodName", function (value, element) {
-//         return this.optional(element) || /^[\sаА-яЯіІєЄїЇґҐa-zA-Z0-9._-]{2,30}$/i.test(value);
-//     }, "Поле заповнено неправильно");
-//
-//     $.validator.addMethod("goodPhone", function (value, element) {
-//         // return this.optional(element) || /^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/i.test(value);
-//         return this.optional(element) || /^[+]*[0-9]{10,14}$/g.test(value);
-//     }, "Введіть номер у форматі +380 xxx xx xx");
-//
-//     form.validate({
-//         rules: {
-//             name: {
-//                 required: true,
-//                 goodName: true
-//                 // minlength:2,
-//                 // maxLength: 25
-//             },
-//             phone: {
-//                 required: true,
-//                 goodPhone: true
-//             },
-//         },
-//         messages: {
-//             name: {
-//                 required: "Це поле є обов’язкове",
-//                 minlength: "Ім'я не може бути коротше за 2 букви",
-//                 maxLength: "Ім'я не може бути довше за 25 букви"
-//             },
-//             phone: {
-//                 required: "Це поле є обов’язкове",
-//                 phone: "Введіть номер у форматі +380 xxx xx xx"
-//             },
-//         },
-//         submitHandler: function () {
-//             func();
-//             form[0].reset();
-//         }
-//     });
-// };
+
+
+const validateForm = (form, func) => {
+    form.on("submit", function (e) {
+        e.preventDefault();
+    });
+
+    $.validator.addMethod("goodName", function (value, element) {
+        return this.optional(element) || /^[\sаА-яЯіІєЄїЇґҐa-zA-Z0-9._-]{2,30}$/i.test(value);
+    }, "Поле заповнено неправильно");
+
+    $.validator.addMethod("goodPhone", function (value, element) {
+        // return this.optional(element) || /^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/i.test(value);
+        return this.optional(element) || /^[+()0-9\s]+$/g.test(value);
+    }, "Введіть номер у форматі +380 (XX) XXX XX XX");
+
+    form.validate({
+        rules: {
+            name: {
+                required: true,
+                goodName: true
+                // minlength:2,
+                // maxLength: 25
+            },
+            phone: {
+                required: true,
+                goodPhone: true
+            },
+        },
+        messages: {
+            name: {
+                required: "Це поле є обов’язкове",
+                minlength: "Ім'я не може бути коротше за 2 букви",
+                maxLength: "Ім'я не може бути довше за 25 букви"
+            },
+            phone: {
+                required: "Це поле є обов’язкове",
+                phone: "Введіть номер у форматі +380 (XX) XXX XX XX"
+            },
+        },
+        submitHandler: function () {
+            func();
+            form[0].reset();
+        }
+    });
+};
+
+
+function ajaxSend(date, url, func,funcError) {
+    $.ajax({
+        url: url,
+        data: date,
+        method: 'POST',
+        success: function (data) {
+            func(data);
+        },
+        error: function (error) {
+            funcError(error)
+
+        },
+        complete: function () {}
+    });
+
+}
+
+// send form
+function sendForm(form, url, func,funcError) {
+    form = form.serialize();
+    ajaxSend(form, url, func,funcError);
+}
+
 
 $(document).ready(function(){
     $('#header__select').select2()
     updateCounter(1, preference.slides.length);
     startTimer();
     changeHeader()
+
+    let contactForm = $('.contact__form');
+    validateForm(contactForm, function () {
+        sendForm(contactForm, '/wp-admin/admin-ajax.php');
+    });
+    $('.contact__phone input').inputmask('+380 (99) 999 99 99');
+
 });
 
 $(window).load(function(){
